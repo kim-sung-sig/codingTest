4d907e9ad439ba2e3e1f9debf22ecd4ec90beb41
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-09-27 09:50:37 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 16132 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int i = 1; i <= n; i++) {
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            System.out.println("Case #" + i + ": " + (a + b));
+        }
+    }
+
+}
