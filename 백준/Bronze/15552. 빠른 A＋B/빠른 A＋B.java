fd0ab3542710ad8beb438bb2d241e3502dc2a071
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-09-27 11:17:53 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1024 ms, Memory: 301696 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < n; i++) {
+            bw.write(Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).sum() + "\n");
+        }
+
+        bw.flush();
+    }
+
+}
