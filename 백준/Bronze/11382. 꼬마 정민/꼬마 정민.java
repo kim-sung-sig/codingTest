7a48a1f04035264c57c4c0c169dccbd3df5deca8
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-09-27 09:55:22 +0900

    [Bronze V] Title: 꼬마 정민, Time: 100 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        long a = Long.parseLong(input[0]);
+        long b = Long.parseLong(input[1]);
+        long c = Long.parseLong(input[2]);
+
+        System.out.println(a + b + c);
+    }
+
+}
