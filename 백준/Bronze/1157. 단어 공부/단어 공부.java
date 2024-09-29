7dbf43f2a804f34ccc615e8a3a8c2d27e7d2b37a
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-09-29 20:28:42 +0900

    [Bronze I] Title: 단어 공부, Time: 240 ms, Memory: 22100 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,35 +1,63 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
         br.close();
 
+        int[] count = new int[26];
         int max = 0;
-        Map<String, Integer> countMap = new HashMap<>();
-        for(String str : input.split("")) {
-            String upperStr = str.toUpperCase();
-            countMap.merge(upperStr, 1, Integer::sum);
-            max = Integer.max(max, countMap.getOrDefault(upperStr, 0));
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c)) {
+                int index = Character.toUpperCase(c) - 'A';
+                count[index]++;
+                max = Math.max(max, count[index]);
+            }
         }
 
         StringBuilder sb = new StringBuilder();
-        for(Map.Entry<String, Integer> entry : countMap.entrySet()) {
-            if(entry.getValue() == max) {
-                if(sb.length() > 0) {
-                    sb.setLength(0);
-                    sb.append("?");
+        boolean multipleMax = false;
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] == max) {
+                if (sb.length() > 0) {
+                    multipleMax = true;
                     break;
                 }
-                sb.append(entry.getKey().toUpperCase());
+                sb.append((char) (i + 'A'));
             }
         }
-        System.out.println(sb.toString());
+
+        System.out.println(multipleMax ? "?" : sb.toString());
     }
+
+    // public static void main(String[] args) throws Exception{
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //     String input = br.readLine();
+    //     br.close();
+
+    //     int max = 0;
+    //     Map<String, Integer> countMap = new HashMap<>();
+    //     for(String str : input.split("")) {
+    //         String upperStr = str.toUpperCase();
+    //         countMap.merge(upperStr, 1, Integer::sum);
+    //         max = Integer.max(max, countMap.getOrDefault(upperStr, 0));
+    //     }
+
+    //     StringBuilder sb = new StringBuilder();
+    //     for(Map.Entry<String, Integer> entry : countMap.entrySet()) {
+    //         if(entry.getValue() == max) {
+    //             if(sb.length() > 0) {
+    //                 sb.setLength(0);
+    //                 sb.append("?");
+    //                 break;
+    //             }
+    //             sb.append(entry.getKey().toUpperCase());
+    //         }
+    //     }
+    //     System.out.println(sb.toString());
+    // }
 
 }
