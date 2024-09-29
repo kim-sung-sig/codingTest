55863019ffb5e8ac95ef1c0799ca80df57de63f7
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-09-29 14:22:10 +0900

    [Bronze II] Title: 나머지, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < 10; i++) {
+            int n = Integer.parseInt(br.readLine());
+            set.add(n % 42);
+        }
+        br.close();
+
+        System.out.println(set.size());
+    }
+
+}
