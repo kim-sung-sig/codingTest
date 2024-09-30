9550411e4eeb7d6720ce9aa887763856634065c8
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-09-30 13:30:32 +0900

    [level 0] Title: 길이에 따른 연산, Time: 4.29 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] num_list) {
+        if( num_list.length>10) {
+        	return Arrays.stream(num_list).sum();
+        } else {
+        	return Arrays.stream(num_list).reduce(1, (x,y)-> x*y);
+        }
+    }
+}
