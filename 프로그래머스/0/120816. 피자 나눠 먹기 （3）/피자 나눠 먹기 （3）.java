14cffcc25a45998089b2c2463a2bbff112e4d0da
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-09-30 13:19:06 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.04 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = n/slice +1;
+        if(n%slice==0){
+            answer = n/slice;
+        }
+        
+        return answer;
+    }
+}
