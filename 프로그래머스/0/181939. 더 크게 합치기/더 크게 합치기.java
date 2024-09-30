d35665f08a2b33e38a77eec60f707569bba1994b
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-09-30 12:03:45 +0900

    [level 0] Title: 더 크게 합치기, Time: 7.62 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int a, int b) {
+        int num1 = Integer.parseInt(a + "" + b);
+		int num2 = Integer.parseInt(b + "" + a);
+		if (num1 >= num2) {
+			return num1;
+		} else {
+			return num2;
+		}
+    }
+}
