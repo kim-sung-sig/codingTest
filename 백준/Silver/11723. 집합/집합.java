09ad57d7dbe1389343103a5f9574a288b2d59e7d
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-10-02 13:43:26 +0900

    [Silver V] Title: 집합, Time: 1116 ms, Memory: 300160 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -3,11 +3,11 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 
 public class Main {
 
     private static int[] arr = new int[21];
+    private static int set = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,35 +19,59 @@ public class Main {
             String cmd = command[0];
             int num = -1;
             switch (cmd) {
-                case "all":
-                    Arrays.fill(arr, 1);
-                    break;
-                case "empty":
-                    Arrays.fill(arr, 0);
-                    break;
                 case "add":
                     num = Integer.parseInt(command[1]);
-                    arr[num] = 1;
+                    set |= (1 << num); // num을 집합에 추가
                     break;
                 case "remove":
                     num = Integer.parseInt(command[1]);
-                    arr[num] = 0;
+                    set &= ~(1 << num); // num을 집합에서 제거
                     break;
                 case "check":
                     num = Integer.parseInt(command[1]);
-                    bw.write(arr[num] + "\n");
+                    bw.write((set & (1 << num)) != 0 ? 1 + "\n": 0 + "\n"); // num의 존재 여부 확인
                     break;
                 case "toggle":
                     num = Integer.parseInt(command[1]);
-                    if(arr[num] == 1) {
-                        arr[num] = 0;
-                    } else {
-                        arr[num] = 1;
-                    }
+                    set ^= (1 << num); // num의 존재 상태를 반전
                     break;
-                default:
+                case "all":
+                    set = (1 << 21) - 1; // 1부터 20까지 모든 비트를 1로 설정
+                    break;
+                case "empty":
+                    set = 0; // 모든 비트를 0으로 설정
                     break;
             }
+            // switch (cmd) {
+            //     case "all":
+            //         Arrays.fill(arr, 1);
+            //         break;
+            //     case "empty":
+            //         Arrays.fill(arr, 0);
+            //         break;
+            //     case "add":
+            //         num = Integer.parseInt(command[1]);
+            //         arr[num] = 1;
+            //         break;
+            //     case "remove":
+            //         num = Integer.parseInt(command[1]);
+            //         arr[num] = 0;
+            //         break;
+            //     case "check":
+            //         num = Integer.parseInt(command[1]);
+            //         bw.write(arr[num] + "\n");
+            //         break;
+            //     case "toggle":
+            //         num = Integer.parseInt(command[1]);
+            //         if(arr[num] == 1) {
+            //             arr[num] = 0;
+            //         } else {
+            //             arr[num] = 1;
+            //         }
+            //         break;
+            //     default:
+            //         break;
+            // }
         }
         br.close();
         bw.flush();
