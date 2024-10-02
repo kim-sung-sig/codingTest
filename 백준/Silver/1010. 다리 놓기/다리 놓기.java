aab282633d8cc2bbff6fed1160f207dce221ee78
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-10-02 12:13:09 +0900

    [Silver V] Title: 다리 놓기, Time: 112 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -24,20 +24,31 @@ public class Main {
 
     }
 
-    // nCr = n-1Cr-1 + n-1Cr (dp를 이용한 컨비네이션)
-    private static int nCr(int n, int r){
-        int[][] dp = new int[n+1][r+1];
-        for(int i = 0; i <= n; i++){
-            for(int j = 0; j <= Math.min(i, r); j++){
-                if(j == 0 || j == i){
-                    dp[i][j] = 1;
-                }else{
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                }
-            }
+    // nCr = n! / r!(n-r)! (팩토리얼을 이용한 컨비네이션)
+    private static int nCr(int n, int r) {
+        int result = 1;
+        for(int i = 0; i < r; i++){
+            result *= (n-i);
+            result /= (i+1);
         }
-        return dp[n][r];
+        return result;
     }
+
+
+    // nCr = n-1Cr-1 + n-1Cr (dp를 이용한 컨비네이션)
+    // private static int nCr(int n, int r){
+    //     int[][] dp = new int[n+1][r+1];
+    //     for(int i = 0; i <= n; i++){
+    //         for(int j = 0; j <= Math.min(i, r); j++){
+    //             if(j == 0 || j == i){
+    //                 dp[i][j] = 1;
+    //             }else{
+    //                 dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+    //             }
+    //         }
+    //     }
+    //     return dp[n][r];
+    // }
 
     // nCr = n-1Cr-1 + n-1Cr (재귀를 이용한 컨비네이션 = 시간초과)
     // private static int nCr(int n, int r){
