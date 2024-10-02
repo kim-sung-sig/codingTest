eaf34fee1f2d09d5d1c1a8695016558e7c8bf00c
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-10-02 12:05:33 +0900

    [Silver V] Title: 다리 놓기, Time: 124 ms, Memory: 15100 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        int[][] sites = new int[t][2];
+        for(int i = 0; i < t; i++){
+            String[] input = br.readLine().split(" ");
+            sites[i][0] = Integer.parseInt(input[0]);
+            sites[i][1] = Integer.parseInt(input[1]);
+        }
+        br.close();
+
+        for(int i = 0; i < t; i++){
+            int n = sites[i][0];
+            int m = sites[i][1];
+            System.out.println(nCr(m, n));
+        }
+
+    }
+
+    // nCr = n-1Cr-1 + n-1Cr (dp를 이용한 컨비네이션)
+    private static int nCr(int n, int r){
+        int[][] dp = new int[n+1][r+1];
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= Math.min(i, r); j++){
+                if(j == 0 || j == i){
+                    dp[i][j] = 1;
+                }else{
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][r];
+    }
+
+    // nCr = n-1Cr-1 + n-1Cr (재귀를 이용한 컨비네이션 = 시간초과)
+    // private static int nCr(int n, int r){
+    //     if(n == r || r == 0){
+    //         return 1;
+    //     }
+    //     return nCr(n-1, r-1) + nCr(n-1, r);
+    // }
+
+}
