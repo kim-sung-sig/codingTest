dca6598f44265822aaad4a917fe3e7f062c527a2
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-10-05 18:47:16 +0900

    [Bronze II] Title: 블랙잭, Time: 100 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -17,26 +17,67 @@ public class Main {
                 .toArray();
         br.close();
 
+        Arrays.sort(arr); // 정렬 후 투 포인터로 접근
+
         int sum = -1;
-        for (int i = 0; i < N - 2; i++) {
 
-            if(arr[i] >= M) continue; // skip
+        for(int i = 0; i < N - 2; i++) {
+            int left = i + 1;
+            int right = N - 1;
 
-            for (int j = i + 1; j < N - 1; j++) {
+            while(left < right) {
+                int tempSum = arr[i] + arr[left] + arr[right];
 
-                if(arr[i] + arr[j] >= M) continue; // skip
+                if(tempSum == M) { // 일치시 얼리 리턴
+                    sum = tempSum;
+                    break;
+                }
 
-                for (int k = j + 1; k < N; k++) {
-                    int tempSum = arr[i] + arr[j] + arr[k];
-
-                    if (tempSum <= M && tempSum > sum) {
-                        sum = tempSum;
-                    }
+                if(tempSum < M) {
+                    sum = Math.max(sum, tempSum);
+                    left++;
+                } else {
+                    right--;
                 }
             }
         }
 
         System.out.println(sum);
+
     }
+
+    // 부루트 포스
+    // public static void main(String[] args) throws IOException {
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //     StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    //     int N = Integer.parseInt(st.nextToken());
+    //     int M = Integer.parseInt(st.nextToken());
+
+    //     int[] arr = Arrays.stream(br.readLine().split(" "))
+    //             .mapToInt(Integer::parseInt)
+    //             .toArray();
+    //     br.close();
+
+    //     int sum = -1;
+    //     for (int i = 0; i < N - 2; i++) {
+
+    //         if(arr[i] >= M) continue; // skip
+
+    //         for (int j = i + 1; j < N - 1; j++) {
+
+    //             if(arr[i] + arr[j] >= M) continue; // skip
+
+    //             for (int k = j + 1; k < N; k++) {
+    //                 int tempSum = arr[i] + arr[j] + arr[k];
+
+    //                 if (tempSum <= M && tempSum > sum) {
+    //                     sum = tempSum;
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     System.out.println(sum);
+    // }
 
 }
