6834db44a9e3d219f3ab906e0a2279a0bca60449
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-10-05 18:37:55 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,37 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] NM = br.readLine().split(" ");
-        int N = Integer.parseInt(NM[0]);
-        int M = Integer.parseInt(NM[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int[] arr = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] arr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
         br.close();
 
         int sum = -1;
-        for(int i = N - 1; i >= 0; i--) {
+        for (int i = 0; i < N - 2; i++) {
 
             if(arr[i] >= M) continue; // skip
 
-            for(int j = i - 1; j >= 0; j--) {
+            for (int j = i + 1; j < N - 1; j++) {
 
                 if(arr[i] + arr[j] >= M) continue; // skip
 
-                for(int k = j - 1; k >= 0; k--) {
+                for (int k = j + 1; k < N; k++) {
                     int tempSum = arr[i] + arr[j] + arr[k];
-                    if(tempSum <= M) {
-                        if(tempSum == M) {
-                            sum = tempSum;
-                            break;
-                        } else if(tempSum > sum) {
-                            sum = tempSum;
-                        }
+
+                    if (tempSum <= M && tempSum > sum) {
+                        sum = tempSum;
                     }
                 }
             }
