21ca2e199b1f33b105c51465ddf4f526e303e883
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-10-12 13:21:32 +0900

    [Bronze II] Title: 손익분기점, Time: 96 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        br.close();
+
+        if(b >= c) {
+            System.out.println(-1);
+        } else {
+            System.out.println(a / (c - b) + 1);
+        }
+    }
+
+}
