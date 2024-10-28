a1b9734449655ed30342d795adb10754bbb2928c
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-10-28 14:12:04 +0900

    [Silver V] Title: 최소공배수, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        System.out.println(lcm(a, b));
+
+        br.close();
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static long lcm(long a, long b) {
+        return a * b / gcd(a, b);
+    }
+}
