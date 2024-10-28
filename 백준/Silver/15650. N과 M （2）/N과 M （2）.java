86e9944731ee1feedbbcb5c2ec6d8822a66d9a51
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-10-28 15:53:05 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 15828 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[] combination;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        br.close();
+
+        combination = new int[M];
+        calc(1, 0);
+    }
+
+    static void calc(int start, int depth) {
+        if (depth == M) {
+            for (int num : combination) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            combination[depth] = i;
+            calc(i + 1, depth + 1); // 시작값보다 큰 값만 선택
+        }
+    }
+
+}
