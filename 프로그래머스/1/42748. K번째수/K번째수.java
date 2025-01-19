9e4070b6086310b5d7b922d24808bcde850e60f4
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2025-01-19 15:18:21 +0900

    [level 1] Title: K번째수, Time: 0.54 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,14 +4,15 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
 
-        for (int r = 0; r < commands.length; r++) {
-            int[] command = commands[r];
+        int r = 0;
+        for (int[] command : commands) {
             int i = command[0], j = command[1], k = command[2];
             int[] temp = Arrays.copyOfRange(array, i - 1, j);
 
             Arrays.sort(temp);
-            answer[r] = temp[k - 1];
+            answer[r++] = temp[k - 1];
         }
+
         return answer;
     }
 }
