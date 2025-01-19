bb072e175adeb14a94fbbaea81e7d80b0a18300e
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2025-01-19 22:27:53 +0900

    [level 2] Title: 숫자 변환하기, Time: 170.90 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -4,35 +4,22 @@ import java.util.Queue;
 
 class Solution {
 
-    int[] dp;   // memoization
-    int d;      // n
-    int goal;   // target
-
     public int solution(int x, int y, int n) {
         if (x == y) return 0;
 
-        dp = new int[y+1];
-        goal = y;
-        d = n;
+        int[] dp = new int[y+1];
+        final int MAX = 1_000_001;
+        Arrays.fill(dp, MAX);
         dp[x] = 0;
 
-        // int answer = solveDfsWrapper(x);
-        int answer = solveBfs(x);
-
-        return answer;
-    }
-
-    int solveBfs(int start) {
         Queue<Integer> q = new LinkedList<>();
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[start] = 0;
-        q.offer(start);
+        q.offer(x);
 
         while (!q.isEmpty()) {
             int cur = q.poll();
 
-            for (int val : new int[]{cur + d, cur * 2, cur * 3}) {
-                if (val > goal) continue;
+            for (int val : new int[]{cur + n, cur * 2, cur * 3}) {
+                if (val > y) continue;
 
                 if(dp[val] > dp[cur] + 1) {
                     dp[val] = dp[cur] + 1;
@@ -41,25 +28,7 @@ class Solution {
             }
         }
 
-        return dp[goal] == Integer.MAX_VALUE ? -1 : dp[goal];
-    }
-
-    int solveDfsWrapper(int start) {
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        solveDfs(start, 0);
-        return dp[goal] == Integer.MAX_VALUE ? -1 : dp[goal];
-    }
-
-    void solveDfs(int cur, int step) {
-        if (cur > goal) return; // 값을 넘어버린 경우
-
-        if (dp[cur] <= step) return; // 더 빠른 방법이 존재하는 경우
-        dp[cur] = step;
-
-        for (int val : new int[]{cur + d, cur * 2, cur * 3}) {
-            if (val > goal) continue;
-            solveDfs(val, step + 1);
-        }
+        return dp[y] == MAX ? -1 : dp[y];
     }
 
 }
