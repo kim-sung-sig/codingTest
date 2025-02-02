b0ac4d4251ba3067d5c0cc6a14822348ab306542
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2025-02-02 21:35:47 +0900

    [Gold V] Title: 치킨 배달, Time: 220 ms, Memory: 20136 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -8,7 +8,8 @@ public class Main {
 
     static int N, M;
     static int[][] map;
-    static List<int[]> chickenRoom = new ArrayList<>();
+    static List<int[]> chickens = new ArrayList<>();
+    static List<int[]> homes = new ArrayList<>();
     static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws Exception {
@@ -23,32 +24,28 @@ public class Main {
             st = new StringTokenizer(br.readLine(), " ");
             for (int j = 0; j < N; j++) {
                 int value = Integer.parseInt(st.nextToken());
-                if (value == 2) chickenRoom.add(new int[]{i, j});
                 map[i][j] = value;
+                if (value == 1) homes.add(new int[]{i, j});
+                else if (value == 2) chickens.add(new int[]{i, j});
             }
         }
 
-        boolean[] visited = new boolean[chickenRoom.size()];
-        dfs(new ArrayList<>(), visited, 0);
+        dfs(new ArrayList<>(), 0);
 
         System.out.println(min);
     }
 
-    static void dfs(List<Integer> selected, boolean[] visited, int start) {
+    static void dfs(List<Integer> selected, int start) {
         if (selected.size() == M) {
             int total = totalChickenDistance(selected);
             min = Math.min(min, total);
             return;
         }
 
-        for (int i = start; i < chickenRoom.size(); i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                selected.add(i);
-                dfs(selected, visited, i + 1);
-                selected.remove(selected.size() - 1);
-                visited[i] = false;
-            }
+        for (int i = start; i < chickens.size(); i++) {
+            selected.add(i);
+            dfs(selected, i + 1);
+            selected.remove(selected.size() - 1);
         }
 
     }
@@ -59,18 +56,16 @@ public class Main {
 
     static int totalChickenDistance(List<Integer> selected) {
         int total = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (map[i][j] == 1) {
-                    int minDistance = Integer.MAX_VALUE;
-                    for (int index : selected) {
-                        int distance = getDistance(new int[]{i, j}, chickenRoom.get(index));
-                        minDistance = Math.min(minDistance, distance);
-                    }
-                    total += minDistance;
-                }
+
+        for (int[] home : homes) {
+            int minDistance = Integer.MAX_VALUE;
+            for (int index : selected) {
+                int distance = getDistance(home, chickens.get(index));
+                minDistance = Math.min(minDistance, distance);
             }
+            total += minDistance;
         }
+
         return total;
     }
 
