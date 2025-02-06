341591a6fcc787a45dca4b6d3d08bc2cc5b65904
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2025-02-06 22:22:48 +0900

    [Silver V] Title: 거스름돈, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        while (n >= 0) {
+            if (n % 5 == 0) {
+                count += n / 5;
+                System.out.println(count);
+                return;
+            }
+            n -= 2;
+            count++;
+        }
+        System.out.println(-1);
+    }
+
+}
