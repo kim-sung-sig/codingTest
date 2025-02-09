ac3bcc553d4f3c299c43c21c970c3e0939d62c29
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2025-02-09 11:18:46 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 108 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,36 +1,31 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] length = br.lines().limit(9).mapToInt(Integer::parseInt).toArray();
 
-        int total = Arrays.stream(length).sum();
-        int[] exclude = new int[2];
+        List<Integer> length = new ArrayList<>();
 
+        br.lines().limit(9).mapToInt(Integer::parseInt).forEach(length::add);
+
+        int total = length.stream().mapToInt(Integer::valueOf).sum();
+
+        outer:
         for (int i = 0; i < 8; i++) {
             for (int j = i + 1; j < 9; j++) {
-                int sum = total - length[i] - length[j];
-                if (sum == 100) {
-                    exclude[0] = i;
-                    exclude[1] = j;
-                    break;
+                if (total - length.get(i) - length.get(j) == 100) {
+                    length.remove(j);
+                    length.remove(i);
+                    break outer;
                 }
             }
         }
 
-        int[] result = new int[7];
-        int c = 0;
-        for (int i = 0; i < 9; i++) {
-            if (i == exclude[0] || i == exclude[1]) continue;
-            result[c++] = length[i];
-        }
-
-        Arrays.sort(result);
-        for (int i : result) System.out.println(i);
+        length.stream().sorted().forEach(System.out::println);
     }
 
 }
