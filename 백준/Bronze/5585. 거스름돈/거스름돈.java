6ade6092d9a6447aa32f1e2edfe088f8fe1cdcbf
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2025-02-26 08:18:47 +0900

    [Bronze II] Title: 거스름돈, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int 거스름돈 = 1000 - n;
+
+        int count = 0;
+        int[] 동전 = { 500, 100, 50, 10, 5, 1 };
+        for (int i = 0; i < 동전.length; i++) {
+            count += 거스름돈 / 동전[i];
+            거스름돈 %= 동전[i];
+        }
+
+        System.out.println(count);
+    }
+
+}
