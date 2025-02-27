e95dc2fb825ef3ed26798abce2b9da8459befc14
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2025-02-28 08:27:40 +0900

    [Bronze I] Title: 약수, Time: 108 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        IntStream.range(0, n).forEach(i -> arr[i] = Integer.parseInt(st.nextToken()));
+        br.close();
+
+        if (arr.length == 1) {
+            System.out.println((int) Math.pow(arr[0], 2));
+            return;
+        }
+
+        Arrays.sort(arr);
+        System.out.println(arr[0] * arr[arr.length - 1]);
+    }
+
+}
