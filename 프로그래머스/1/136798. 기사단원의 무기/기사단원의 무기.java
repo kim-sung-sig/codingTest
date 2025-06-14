e6202fa581b5c4ca86a64e09be4947af2457276e
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2025-06-14 21:13:41 +0900

    [level 1] Title: 기사단원의 무기, Time: 11.40 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -2,24 +2,21 @@ class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
 
+        // 에라토스테네스 체
+        int[] divisorCount = new int[number + 1];
         for (int i = 1; i <= number; i++) {
-            int numberOfDivisors = countDivisors(i);
+            for (int j = i; j <= number; j += i) {
+                divisorCount[j]++;
+            }
+        }
 
-            answer += limit < numberOfDivisors
+        for (int i = 1; i <= number; i++) {
+            int numberOfDivisors = divisorCount[i];
+            answer += (numberOfDivisors > limit)
                     ? power
                     : numberOfDivisors;
         }
 
         return answer;
-    }
-
-    public int countDivisors(int n) {
-        int count = 0;
-        for (int i = 1; i * i <= n; i++) {
-            if (n % i == 0) {
-                count += (i * i == n) ? 1 : 2;
-            }
-        }
-        return count;
     }
 }
