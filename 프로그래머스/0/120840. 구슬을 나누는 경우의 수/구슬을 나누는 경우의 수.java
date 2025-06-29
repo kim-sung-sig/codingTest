a43cd3a35cc66128beecae7ed640743972a88653
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2025-06-29 22:44:08 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 2.13 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -1,10 +1,47 @@
+import java.util.*;
+
 class Solution {
+    Map<nCr, Integer> memo = new HashMap<>();
+
     public int solution(int balls, int share) {
         return combination(balls, share);
     }
-    
+
     public int combination(int n, int r) {
         if (r == 0 || n == r) return 1;
-        return combination(n - 1, r - 1) + combination(n - 1, r);
+
+        nCr key = new nCr(n, r);
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+
+        int value = combination(n - 1, r - 1) + combination(n - 1, r);
+        memo.put(key, value);
+
+        return value;
+    }
+}
+
+
+class nCr {
+    int n;
+    int r;
+
+    public nCr(int n, int r) {
+        this.n = n;
+        this.r = r;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof nCr)) return false;
+        nCr other = (nCr) o;
+        return n == other.n && r == other.r;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(n, r);
     }
 }
