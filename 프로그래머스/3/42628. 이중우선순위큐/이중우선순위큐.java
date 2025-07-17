cb3de8104e82a7fc7944ee50ca8b3a0f480ba88f
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2025-07-18 00:17:05 +0900

    [level 3] Title: 이중우선순위큐, Time: 86.14 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -3,16 +3,16 @@ import java.util.TreeMap;
 
 class Solution {
 	public int[] solution(String[] operations) {
-		TreeMap<Integer, Integer> map = new TreeMap<>();
-		for (String op : operations) {
-			String[] arr = op.split(" ");
-			String o = arr[0];
-			int num = Integer.parseInt(arr[1]);
+		var map = new TreeMap<Integer, Integer>();
+		for (var op : operations) {
+			var arr = op.split(" ");
+			var o = arr[0];
+			var num = Integer.parseInt(arr[1]);
 
 			if (o.equals("I")) map.merge(num, 1, Integer::sum);
 			else if (!map.isEmpty()) {
 				var key = num < 0 ? map.firstKey() : map.lastKey();
-				map.computeIfPresent(key, (k, v) -> v == 1 ? null : v - 1);
+				map.computeIfPresent(key, (k, v) -> v > 1 ? v - 1 : null);
 			}
 		}
 
