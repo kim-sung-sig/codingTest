a06c9ddce94638e2bc286e9518cd236c1b635dcf
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2025-07-31 00:47:54 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,17 +1,16 @@
 class Solution {
 	public int solution(int n, int w, int num) {
-		int answer = 0;
-
 		int floor = (num - 1) / w + 1; // 물건의 층수
-		int tempIndex = (num - 1) % w;
-		int index = floor % 2 == 0 ? (w - 1) - tempIndex : tempIndex;
-
 		int totalFloors = (n - 1) / w + 1; // 최고층수
-		int lastTempIndex = (n - 1) % w;
-		int lastIndex = totalFloors % 2 == 0 ? (w - 1) - lastTempIndex : lastTempIndex;
 
 		// 최고층인 경우
 		if (floor == totalFloors) return 1;
+
+		int tempIndex = (num - 1) % w;
+		int index = floor % 2 == 0 ? (w - 1) - tempIndex : tempIndex;
+
+		int lastTempIndex = (n - 1) % w;
+		int lastIndex = totalFloors % 2 == 0 ? (w - 1) - lastTempIndex : lastTempIndex;
 
 		int k = (totalFloors % 2 == 0 && index < lastIndex) || (totalFloors % 2 == 1 && lastIndex < index) ? -1 : 0;
 		return totalFloors - floor + 1 + k;
