96094758f67b12501fcd8835a9f5fefaf57a1427
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2025-10-10 19:03:17 +0900

    [level 3] Title: 네트워크, Time: 0.31 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,14 +1,13 @@
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-
         boolean[] visited = new boolean[n];
-        
+
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(computers, visited, i);
-                answer++;
-            }
+            if (visited[i]) continue;
+            
+            dfs(computers, visited, i);
+            answer++;
         }
 
         return answer;
@@ -18,9 +17,10 @@ class Solution {
         visited[node] = true;
 
         for (int i = 0; i < computers.length; i++) {
-            if (computers[node][i] == 1 && !visited[i]) {
-                dfs(computers, visited, i);
-            }
+            if (computers[node][i] != 1) continue;
+            if (visited[i]) continue;
+
+            dfs(computers, visited, i);
         }
     }
 
